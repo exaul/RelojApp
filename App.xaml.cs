5b4c67d6b49a85a2ab14a2dcb585646c11ff04d6
exaul <edwin.falcones@tecnologicoliceocristiano.edu.ec>
2023-11-13 21:28:01 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RelojApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ClockViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelojApp
+{
+    public class ClockViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DateTime _dateTime;
+        private Timer _timer;
+        public DateTime Fechahora
+        {
+            get => _dateTime;
+            set
+            {
+                if (_dateTime != value)
+                {
+                    _dateTime = value;
+                    OnPropertyChanged(); // reports this property
+                }
+            }
+        }
+        public ClockViewModel()
+        {
+            this.Fechahora = DateTime.Now;
+
+            // Update the DateTime property every second.
+            _timer = new Timer(new TimerCallback((s) => this.Fechahora = DateTime.Now),
+                               null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        }
+        ~ClockViewModel() =>
+        _timer.Dispose();
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
+   
